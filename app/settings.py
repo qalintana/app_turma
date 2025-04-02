e52e14bac867fc5469f917c40e966cf219961679
Queiroz Santana <alintana18@gmail.com>
2025-04-02 18:45:29 +0100

    adicionando bootstrap

--- app/settings.py
+++ app/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'atendimento'
 ]
 
 MIDDLEWARE = [
@@ -105,6 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- atendimento/views.py
+++ atendimento/views.py
@@ -1,3 +1,13 @@
+
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def pagina_inicial(request):
+  return render(request, 'index.html')
+
+
+def site(request):
+  return HttpResponse("<b>viva a paz</b>")
